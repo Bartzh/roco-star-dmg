2d7bf4a50d77a81623962b0742c48cfeb4be0a23
Bart <81236786+Bartzh@users.noreply.github.com>
2026-07-23 18:31:33 +0800

    更新数据（六自由度削弱）

--- datas/src/skill_catalog.lua
+++ datas/src/skill_catalog.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16b4c218d62bdaa1ec2178088fdf1289eddbda985ca9e7138e880737ab92858e
+oid sha256:545ae7dd26a795a6792aab874d768430a82eadef3df17e45843dbcec95407c8b
 size 178185
