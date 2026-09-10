e03a6d3e84d0e1c986e249057a272ed8863ebcef
Bart <81236786+Bartzh@users.noreply.github.com>
2026-09-10 17:09:47 +0800

    更新文字说明和链接

--- calculator.js
+++ calculator.js
@@ -4698,8 +4698,8 @@ const MODAL_CONTENT = {
 
       <h3>数据来源</h3>
       <ul>
-        <li>精灵与技能数据来自 <a href="https://wiki.biligame.com/rocom" target="_blank" rel="noopener noreferrer">BWIKI</a>。</li>
-        <li>数据更新于 2026-08-24。</li>
+        <li>精灵与技能数据来自 <a href="https://wiki.biligame.com/nrc" target="_blank" rel="noopener noreferrer">BWIKI</a>。</li>
+        <li>数据更新于 2026-09-10。</li>
       </ul>
 
       <h3>关于</h3>
